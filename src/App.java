b0aef92e1b80fa3f8bde4a56b1af73e201b8c46b
GZZZ <walter.gz@icloud.com>
2024-11-10 18:21:52 -0600

    Menu para testing

--- src/App.java
+++ src/App.java
@@ -1,12 +1,43 @@
 import Stack.StackLinked;
-
+import Ejercicios.Agenda;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        EstabloTest();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int opcion;
+
+        do {
+            System.out.println("\n=== MENU PRINCIPAL ===");
+            System.out.println("1. Test Agenda");
+            System.out.println("2. Test Establo");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            try {
+                opcion = Integer.parseInt(reader.readLine());
+                switch (opcion) {
+                    case 1:
+                        Agenda agenda = new Agenda();
+                        agenda.test();
+                        break;
+                    case 2:
+                        EstabloTest();
+                        break;
+                    case 0:
+                        System.out.println("¡Hasta luego!");
+                        break;
+                    default:
+                        System.out.println("Opción no válida");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                opcion = -1;
+            }
+        } while (opcion != 0);
+
+        reader.close();
     }
     
     public static void EstabloTest() {
@@ -21,8 +52,6 @@ public class App {
                 Double peso = Double.parseDouble(reader.readLine());
                 establo.push(peso);
             }
-
-            reader.close();
             
             System.out.println("Establo original: " + establo);
             StackLinked<Double> establoOrdenado = establo.sort();
